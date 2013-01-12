a8ea9eee37c3cba75c057f25ac1b5e3907eeac99
JingWZ <nj2408zjw@hotmail.com>
2013-01-12 13:56:07 +0800

    Initial Commit

---
+++ MicrophoneViewDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MicrophoneViewDemo
+//
+//  Created by apple on 13-1-12.
+//  Copyright (c) 2013年 jing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MicrophoneViewDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MicrophoneViewDemo
+//
+//  Created by apple on 13-1-12.
+//  Copyright (c) 2013年 jing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
